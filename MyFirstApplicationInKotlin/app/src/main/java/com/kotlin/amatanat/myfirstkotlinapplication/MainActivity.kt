8ed9551057afca2dadb457ae6b81741b286f61c3
Matanat Ahmadova <ahmadova.matanat01@gmail.com>
2017-08-12 14:40:33 +0200

    first commit - example app in Kotlin

---
+++ MyFirstApplicationInKotlin/app/src/main/java/com/kotlin/amatanat/myfirstkotlinapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.kotlin.amatanat.myfirstkotlinapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener({
+            Toast.makeText(this, "SUCCESS", Toast.LENGTH_LONG).show();
+        }
+        )
+    }
+}
+
+
